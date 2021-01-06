42946d6c8e0c2a0886e90e89ea21e4b7f53b9a6a
dagadescartes <engineerindustrial1@gmail.com>
2021-01-06 12:23:12 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,9 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'app Flutter Demo home pageHome Page 2'),
+      home: MyHomePage(
+          title:
+              'demosoljlksadjñlkjsñlkdsagfsa app Flutter Demo home pageHome Page 2'),
     );
   }
 }
